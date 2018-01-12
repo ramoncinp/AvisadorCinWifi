c0555940bacbc2395d91f4c0ff7bd2f5376cbd35
AndresDiseno <andresmartinez@supramax.com.mx>
2018-01-12 11:02:41 -0600

    Proyecto AvisadorCinWifiClient agregado

---
+++ AvisadorCinWifiClient/AvisadorCinWifiClient.ino
@@ -0,0 +1,20 @@
+/*
+  AvisadorCinWifiClient
+
+  Descripción: Activar el led conectado a un servidor http a través de un request
+  generado por un cliente, el request deberá realizarse al presionar un botón 
+  conectado en una entrada digital del cliente
+
+  Programa: Cliente Http
+  //TODO: El cliente :o
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
