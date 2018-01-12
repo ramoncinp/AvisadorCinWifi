bfb2145ee6730762817e64c3ac1432bcdaa24645
RAMON PARRA <aldairparrag@gmail.com>
2018-01-12 09:31:21 -0600

    Propuesta de proyecto

---
+++ AvisadorCinWifiServer/AvisadorCinWifiServer/AvisadorCinWifiServer.ino
@@ -0,0 +1,20 @@
+/*
+  AvisadorCinWifi
+
+  Descripción: Activar el led conectado a un servidor http a través de un request
+  generado por un cliente, el request deberá realizarse al presionar un botón 
+  conectado en una entrada digital del cliente
+
+  Programa: Servidor Http
+  //TODO: El servidor :v
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
